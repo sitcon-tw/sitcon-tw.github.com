a8d11ec06d404e2b5210ee95dc7697558740d8f7
Denny Huang <denny0223@gmail.com>
2025-03-21 12:59:50 +0800

    Add GTM to build.js

--- build.js
+++ build.js
@@ -10,6 +10,13 @@ links.forEach(({ paths, url, title, description }) => {
         const htmlContent = `<!DOCTYPE html>
 <html lang="zh-Hant">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NPVBCDZ');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="refresh" content="0; url=${url}">
@@ -23,6 +30,10 @@ links.forEach(({ paths, url, title, description }) => {
     <link rel="canonical" href="${url}">
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPVBCDZ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <p>正在前往 <a href="${url}">${title}</a></p>
 </body>
 </html>`;
